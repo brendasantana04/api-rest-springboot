a95bff150ef5c04e43653ccff3971bbe24e526ef
brendasantana04 <dollanganger.8@gmail.com>
2024-09-11 16:47:33 -0300

    iniciando projeto de api com springboot

---
+++ src/main/java/me/dio/api_rest_springboot/ApiRestSpringbootApplication.java
@@ -0,0 +1,13 @@
+package me.dio.api_rest_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/api_rest_springboot/ApiRestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.api_rest_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
